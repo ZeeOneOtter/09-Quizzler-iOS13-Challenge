3c89c99b000cc69b033265787719971b2afc530b
ZeeOneOtter <61956050+ZeeOneOtter@users.noreply.github.com>
2023-11-27 00:31:26 -0800

    small bug that required double pressing of button to progres. or at least in the beginning. cleaned up my code a bit more to align with the result of a true/false Quizzler example

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -44,28 +44,40 @@ class ViewController: UIViewController {
 
     @IBAction func answerButtonPressed(_ sender: UIButton) {
         
+        //keep track of question Label, and total questions asked
+//        updateUI()
+        
         let userAnswer = sender.currentTitle
         
-        if questionNumber < quiz.count {
+        if questionNumber + 1 < quiz.count {
             
             let actualAnswer = quiz[questionNumber].answer /* actual answer variable needs to be stored here. Because questionNumber += 1 will happen, and although the if statement is such that it will continue onto else-part of the statement, the issue arises when the player presses the button another time. The variable questionNumber has now been saved last as something outside the array index by +1, thus if initalized and kept within if-statment then the issue of index error of questionNumber is avoided.*/
-            //another way of accomplishing the above.
+            //another way of accomplishing the above:
 //            let actualQuestion = quiz[questionNumber]
 //            let actualAnswer = actualQuestion.answer
             
             // need to see if user answer matches response
             if userAnswer == actualAnswer {
                 userScore += 1
+                updateUI()
+                sender.backgroundColor = UIColor.green
+                //Code should execute after 0.2 second delay.
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+                    //Bring's sender's opacity back up to fully opaque.
+                    sender.backgroundColor = UIColor.clear
+                }
+            } else {
+                sender.backgroundColor = UIColor.red
             }
             
             //needs to be the last thing to do as we need to record user answer to correct array question/answer pairing.
             questionNumber += 1
-            //keep track of question Label, and total questions asked
             updateUI()
         } else {
             questionLabel.text = "Yay! You've completed the quiz. \nYour Score is: \(userScore) of \(quiz.count).\nPress any button to restart the quiz."
             quiz.shuffle()
-            progressBar.progress = 0.0
+            progressBar.progress = 1.0
+            progressBar.progressTintColor = UIColor.yellow
             questionNumber = 0
             userScore = 0
             
@@ -75,17 +87,15 @@ class ViewController: UIViewController {
     
     func updateUI() {
         
-        //progresBar needs to have float inputs becasue doing division on integers results in a float but rounding errors. For instance, int(5)/int(2) = 2.0 because the numbers are integers whne the calculation is done, the proper answer of 2.5 is not computed.
+        //progresBar needs to have float inputs because doing division on integers results in a float but rounding errors. For instance, int(5)/int(2) = 2.0 because the numbers are integers whne the calculation is done, the proper answer of 2.5 is not computed.
         let totalNumberOfQuestions = Float(quiz.count)
         let floatQuestionNumber = Float(questionNumber)
-        var percentQuizComplete = floatQuestionNumber/totalNumberOfQuestions
+        let percentQuizComplete = floatQuestionNumber/totalNumberOfQuestions
         progressBar.progress = percentQuizComplete
         
-        if floatQuestionNumber < 1 {
-            questionLabel.text = quiz[questionNumber].text
-        }
-        
 
+        questionLabel.text = quiz[questionNumber].text
+        
     }
     
     
